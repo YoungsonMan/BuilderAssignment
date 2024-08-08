b7418009e4fa229f136fb9effe427ae702142e37
YoungsonMan <choi13570@naver.com>
2024-08-09 00:36:40 +0900

    [Add] Make more cars
    Added more cars using chain method

--- Program.cs
+++ Program.cs
@@ -18,26 +18,43 @@
             porcheBuilder.SetBrand("Porche");
             porcheBuilder.SetName("911");
             porcheBuilder.SetPrice(114400);
-            porcheBuilder.SetEngine("3.6 L 6-cylinder");
+            porcheBuilder.SetEngine("3.6L 6기통");
             porcheBuilder.SetHorsePower("388 to 478 hp");
             porcheBuilder.SetTransmission("7 & 8-speed automatic");
             porcheBuilder.SetZeroSixty(3.50d);
             Car car0 = porcheBuilder.Build();
-            Console.WriteLine($"{car0.year}{car0.brand} {car0.name}\t가격:${car0.price}\t엔진:{car0.engine}\t마력:{car0.horsePower}\t변속기:{car0.transmission}\t제로백:{car0.zeroSixty}");
+            Console.WriteLine($"{car0.year} {car0.brand} {car0.name}, 가격:${car0.price}, 엔진:{car0.engine}, 마력:{car0.horsePower}, 변속기:{car0.transmission}, 제로백:{car0.zeroSixty}");
 
 
             CarBuilder lamboBuilder = new CarBuilder();
             lamboBuilder.SetYear(2024).SetBrand("Lamborghini").SetName("Huracan").SetPrice(249685).SetEngine("5.2 L V10").SetHorsePower("640 hp").SetTransmission("7-speed automatic").SetZeroSixty(2.90d);
+            CarBuilder genesisBuilder = new CarBuilder();
+            genesisBuilder.SetYear(2024).SetBrand("Genesis").SetName("GV80").SetPrice(64850).SetEngine("3.5 L V6").SetHorsePower("300 to 375 hp").SetTransmission("8-speed automatic").SetZeroSixty(6.10d);
+            CarBuilder bmwBuilder = new CarBuilder();
+            bmwBuilder.SetYear(2024).SetBrand("BMW").SetName("M4").SetPrice(78100).SetEngine("3.0 L 6기통").SetHorsePower("473 to 503 hp").SetTransmission("8-speed automatic").SetZeroSixty(3.80d);
+            CarBuilder mercedesBuilder = new CarBuilder();
+            mercedesBuilder.SetYear(2024).SetBrand("Mercedes-Benz").SetName("AMG GT").SetPrice(134900).SetEngine("4.0 L V8").SetHorsePower("577 hp").SetTransmission("9-speed automatic").SetZeroSixty(3.10d);
+            CarBuilder mcLarenBuilder = new CarBuilder();
+            mcLarenBuilder.SetYear(2024).SetBrand("McLaren").SetName("750S").SetPrice(324000).SetEngine("4.0 L V8").SetHorsePower("740 hp").SetTransmission("7-speed automatic").SetZeroSixty(2.70d);
 
 
+            Car[] cars = new Car[5];
+            cars[0] = lamboBuilder.Build();
+            cars[1] = genesisBuilder.Build();
+            cars[2] = bmwBuilder.Build();
+            cars[3] = mercedesBuilder.Build();
+            cars[4] = mcLarenBuilder.Build();
 
-            Car[] cars = new Car[4];
-
+            for (int i = 0; i < cars.Length; i++)
+            {
+                //Console.WriteLine($"{i}번 자동차: ");
+                Console.WriteLine($"{cars[i].year} {cars[i].brand} {cars[i].name}, 가격:${cars[i].price}, 엔진:{cars[i].engine}, 마력:{cars[i].horsePower}, 변속기:{cars[i].transmission}, 제로백:{cars[i].zeroSixty}");
+            }
 
 
 
         }
     }
-    
-    
+
+
 }
