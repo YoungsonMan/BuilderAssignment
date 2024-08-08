a91d0da8665b9c41cd481427b821746e4af652a1
YoungsonMan <choi13570@naver.com>
2024-08-09 00:10:32 +0900

    [Add] Builder Pattern
    Builder pattern has been addded so as some examples.

---
+++ CarBuilder.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderAssignment
+{
+    public class Car
+    {
+        public int year;
+        public string brand;
+        public string name;
+        public int price;
+        public string engine;
+        public string horsePower;
+        public string transmission;
+        public double zeroSixty;
+    }
+    public class CarBuilder
+    {
+        public int year;
+        public string brand;
+        public string name;
+        public int price;
+        public string engine;
+        public string horsePower;
+        public string transmission;
+        public double zeroSixty;
+
+        public CarBuilder()
+        {
+            this.year = 2024;
+            this.brand = "brand";
+            this.name = "car";
+            this.price = 10000;
+            this.engine = "engine";
+            this.horsePower = "100 to 200";
+            this.transmission = "4-speed automatic";
+            this.zeroSixty = 10.35f;
+        }
+        public Car Build()
+        {
+            Car car = new Car();
+            car.year = year;
+            car.brand = brand;
+            car.name = name;
+            car.price = price;
+            car.engine = engine;
+            car.horsePower = horsePower;
+            car.transmission = transmission;
+            car.zeroSixty= zeroSixty;
+            return car;
+        }
+        public CarBuilder SetYear(int year)
+        {
+            this.year = year;
+            return this;
+        }
+        public CarBuilder SetBrand(string brand)
+        {
+            this.brand = brand;
+            return this;
+        }
+        public CarBuilder SetName(string name)
+        {
+            this.name = name;
+            return this;
+        }
+        public CarBuilder SetPrice(int price)
+        {
+            this.price = price;
+            return this;
+        }
+        public CarBuilder SetEngine(string engine)
+        {
+            this.engine = engine;
+            return this;
+        }
+        public CarBuilder SetHorsePower(string horsePower)
+        {
+            this.horsePower = horsePower;
+            return this;
+        }
+        public CarBuilder SetTransmission(string transmission)
+        {
+            this.transmission = transmission;
+            return this;
+        }
+        public CarBuilder SetZeroSixty(double zeroSixty)
+        {
+            this.zeroSixty = zeroSixty;
+            return this;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace BuilderAssignment
+{/**************************************************************************
+ * 디자인패턴 과제
+ * YC
+ * 과제 2. 빌더패턴 활용하기
+ * 원하는 Builder Class를 만들고, 빌더를 통해서 다양한 객체를 생성해보기
+ * 예시: 
+ *      MonsterBuilder Class를 만들어 몬스터의 이름, 외형, 전리품, 공격범위, 전투스타일 등을 조합하여 여러유형의 몬스터를 만든다.
+ *      AnimalBuilder Class를 만들어 동물의 이름, 외형, 생산품, 울음소리, 사료종류 등을 조합하여 여러 유형의 동물들을 만든다.
+ *      UnitBuilder Class를 유닛의 이름, 이동방법, 공격타입, 방어타입, 생산가격 등을 조합하여 여러 유형의 유닛들을 만들수 있다.
+ **************************************************************************/
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CarBuilder porcheBuilder = new CarBuilder();
+            porcheBuilder.SetYear(2024);
+            porcheBuilder.SetBrand("Porche");
+            porcheBuilder.SetName("911");
+            porcheBuilder.SetPrice(114400);
+            porcheBuilder.SetEngine("3.6 L 6-cylinder");
+            porcheBuilder.SetHorsePower("388 to 478 hp");
+            porcheBuilder.SetTransmission("7 & 8-speed automatic");
+            porcheBuilder.SetZeroSixty(3.50d);
+            Car car0 = porcheBuilder.Build();
+            Console.WriteLine($"{car0.year}{car0.brand} {car0.name}\t가격:${car0.price}\t엔진:{car0.engine}\t마력:{car0.horsePower}\t변속기:{car0.transmission}\t제로백:{car0.zeroSixty}");
+
+
+            CarBuilder lamboBuilder = new CarBuilder();
+            lamboBuilder.SetYear(2024).SetBrand("Lamborghini").SetName("Huracan").SetPrice(249685).SetEngine("5.2 L V10").SetHorsePower("640 hp").SetTransmission("7-speed automatic").SetZeroSixty(2.90d);
+
+
+
+            Car[] cars = new Car[4];
+
+
+
+
+        }
+    }
+    
+    
+}
